53e6d7abb4f660b55c09e99cee0d242c3c81e23d
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-06-27 00:41:38 +0200

    * upgrade cocktail creation form.

--- HappyHours/Scripts/HhJavascripts/Views/CocktailCreation.js
+++ HappyHours/Scripts/HhJavascripts/Views/CocktailCreation.js
@@ -16,47 +16,82 @@ define([
             base.app = app;
 
             base.render();
-            base.registerEvents();
         },
         render:function () {
             var base = this;
             var template = _.template(CocktailCreationTemplate, {});
             base.$el.html(template);
+            base.registerEvents();
         },
         registerEvents:function () {
             var base = this;
+            $(document).ready(function () {
+                base.$el.find("#creation_form_name").focus();
+            });
+
+            base.$el.delegate(".form_cocktail input", "click", function () {
+                var elt = $(this);
+                console.log("click");
+                elt.removeClass("error_submit");
+            });
+
             var form = base.$el.find(".form_cocktail");
             form.submit(function () {
                 base.$el.find(".form_container").hide();
                 base.$el.find(".loader").show();
 
-                base.formSubmission(function () {
+                if (base.checkTextFields()) {
+                    base.formSubmission(function () {
+                        base.$el.find(".loader").fadeOut(100);
+                        base.$el.find(".form_container").show();
+                    });
+                }
+                else {
                     base.$el.find(".loader").fadeOut(100);
                     base.$el.find(".form_container").show();
-                });
+                }
             });
         },
         formSubmission:function (callback) {
             var base = this;
-
             var iframe = base.$el.find(".form_iframe");
             iframe.load(function () {
-                console.log("iframe.contents()", iframe.contents());
-                console.log("iframe.contents()find(pre)", iframe.contents().find("pre"));
-                console.log("iframe.contents()find(pre).html()", iframe.contents().find("pre").html());
-
                 var iframeHtml = iframe.contents().find("pre").html();
                 if (iframeHtml !== undefined) {
-                    var object = JSON.parse(iframe.contents().find("pre").html());
-                    console.log("object", object);
+                    var object = JSON.parse(iframeHtml);
                     var cocktail = new Cocktail(object);
-                    console.log("cocktail", cocktail);
                     base.app.router.navigate("#cocktail/" + cocktail.get("Id"), { trigger:true })
                 }
             });
 
             if (callback)
                 callback();
+        },
+        checkTextFields:function () {
+            var base = this;
+            var canSubmit = true;
+            console.log("checkTextFields");
+            if (base.$el.find("#creation_form_name").val() == "") {
+                canSubmit = false;
+                base.$el.find("#creation_form_name").addClass("error_submit");
+            }
+            if (base.$el.find("#creation_form_difficulty").val() == "") {
+                canSubmit = false;
+                base.$el.find("#creation_form_difficulty").addClass("error_submit");
+            }
+            if (base.$el.find("#creation_form_duration").val() == "") {
+                canSubmit = false;
+                base.$el.find("#creation_form_duration").addClass("error_submit");
+            }
+            if (base.$el.find("#creation_form_description").val() == "") {
+                canSubmit = false;
+                base.$el.find("#creation_form_description").addClass("error_submit");
+            }
+            if (base.$el.find("#creation_form_recipe").val() == "") {
+                canSubmit = false;
+                base.$el.find("#creation_form_recipe").addClass("error_submit");
+            }
+            return canSubmit;
         }
     });
 
--- HappyHours/Scripts/HhJavascripts/default.js
+++ HappyHours/Scripts/HhJavascripts/default.js
@@ -4,7 +4,12 @@
 
     var timer = 0;
     var defaults = {
-        show_message: function (msg) {
+        isNormalInteger:function (str) {
+            var intRegex = /^\d+$/;
+            return (intRegex.test(str));
+//            return /^\+?(0|[1-9]\d*)$/.test(str);
+        },
+        show_message:function (msg) {
             clearTimeout(timer);
             $("#flash_message").html(msg);
             $("#flash_shower").fadeIn(300);
