6898f5118f7b46fc051de6c85ec713a4e5fb5504
Antoine Jackson <a.j.william26@gmail.com>
2013-06-28 00:56:15 +0200

    * Added backend

---
+++ HhBackend/BusinessManagement/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HhBackend.BusinessManagement
+{
+    public class User
+    {
+        public static bool ValidateUser(string username, string password)
+        {
+            return DataAccess.User.ValidateUser(username, password);
+        }
+
+        public static HhDBO.User GetUser(string username)
+        {
+            return DataAccess.User.GetUser(username);
+        }
+    }
+
+}
---
+++ HhBackend/DataAccess/User.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HhBackend.DataAccess
+{
+    public class User
+    {
+        public static bool ValidateUser(string username, string password)
+        {
+            bool response = false;
+            using (HappyHoursEntities ctx = new HappyHoursEntities())
+            {
+                response = ctx.T_User.Where(x => x.username == username && x.password == password).ToList().Any();
+            }
+            return response;
+        }
+
+        public static HhDBO.User GetUser(string username)
+        {
+            HhDBO.User user = null;
+
+            using (HappyHoursEntities ctx = new HappyHoursEntities())
+            {
+                Mapper.CreateMap<T_User, HhDBO.User>();
+                T_User t = ctx.T_User.Where(x => x.username == username).FirstOrDefault();
+                if (t != null)
+                    user = Mapper.Map<T_User, HhDBO.User>(t);
+
+            }
+
+            return user;
+        }
+    }
+}
