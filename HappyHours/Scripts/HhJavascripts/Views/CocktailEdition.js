227bfbc0188ed4000c30cd668f6576341e74f6a7
Antoine Jackson <a.j.william26@gmail.com>
2013-06-29 01:51:51 +0200

    * Added filter to edition page

--- HappyHours/Scripts/HhJavascripts/Views/CocktailEdition.js
+++ HappyHours/Scripts/HhJavascripts/Views/CocktailEdition.js
@@ -10,25 +10,25 @@ define([
     'jqueryui'
 ], function ($, _, Backbone, Cocktail, CocktailEditionTemplate, IngredientsCollection, IngredientItemView, CocktailImageView) {
     var CocktailEditionView = Backbone.View.extend({
-        tagName:"div",
-        className:"cocktail_edition_view",
-        initialize:function () {
+        tagName: "div",
+        className: "cocktail_edition_view",
+        initialize: function () {
             var base = this;
         },
-        init:function (app, id) {
+        init: function (app, id) {
             var base = this;
             base.app = app;
 
 
             base.ingredients_collection = new IngredientsCollection();
-            base.cocktail = new Cocktail({ Id : id});
+            base.cocktail = new Cocktail({ Id: id});
 
             base.cocktail.fetch({
-                success:function () {
+                success: function () {
                     base.cocktail_image = new CocktailImageView(base.cocktail);
                     if ((!base.app.current_user || base.app.current_user.get("Id") != base.cocktail.get("Creator_Id"))) {
                         base.app.show_message("Vous n'avez pas les droits pour accéder à cette page");
-                        base.app.router.navigate("#cocktail/" + base.cocktail.get("Id"), {trigger: true} );
+                        base.app.router.navigate("#cocktail/" + base.cocktail.get("Id"), {trigger: true});
                     }
 
                     base.render();
@@ -36,10 +36,10 @@ define([
                 }
             });
         },
-        render:function () {
+        render: function () {
             var base = this;
             var template = _.template(CocktailEditionTemplate, {
-                cocktail:base.cocktail
+                cocktail: base.cocktail
             });
 
             base.$el.html(template);
@@ -49,7 +49,7 @@ define([
             base.cocktail_image.init(base.app);
 
             base.ingredients_collection.fetch({
-                success:function () {
+                success: function () {
                     var list = base.ingredients_collection.models;
                     base.$el.find(".all_ingredients").find(".ingredient_item").remove();
                     for (var k in list) {
@@ -69,22 +69,22 @@ define([
 
             console.log(base.cocktail);
             base.$el.find(".all_ingredients").sortable({
-                connectWith:".ingredients_list",
-                receive:function (event, ui) {
+                connectWith: ".ingredients_list",
+                receive: function (event, ui) {
                     base.cocktail.get("Ingredients").remove(base.ingredients_collection.get(ui.item.attr("data-id")));
                     console.log("base.cocktail.get(Ingredients)", base.cocktail.get("Ingredients"));
                 }
             });
 
             base.$el.find(".selected_ingredients").sortable({
-                connectWith:".ingredients_list",
-                receive:function (event, ui) {
+                connectWith: ".ingredients_list",
+                receive: function (event, ui) {
                     base.cocktail.get("Ingredients").add(base.ingredients_collection.get(ui.item.attr("data-id")));
                     console.log("base.cocktail.get(Ingredients)", base.cocktail.get("Ingredients"));
                 }
             });
         },
-        registerEvents:function () {
+        registerEvents: function () {
             var base = this;
 
             base.$el.delegate(".form_cocktail", "submit", function () {
@@ -96,7 +96,7 @@ define([
                 }
 
                 base.cocktail.save({}, {
-                    success:function () {
+                    success: function () {
                         base.app.show_message("Le cocktail a bien été enregistré");
                     },
                     error: function () {
@@ -115,12 +115,24 @@ define([
             });
 
 
-
             base.$el.find(".form_iframe").load(function () {
                 base.cocktail.fetch({
-                    success:function () {
+                    success: function () {
 //                        base.$el.find(".cocktail_picture").attr("src", base.cocktail.get("Picture_Url"));
                         base.app.show_message("L'image du cocktail a bien été mise à jour");
+                    }
+                });
+            });
+
+            base.$el.delegate(".ingredient_filter", "keyup", function () {
+                var search_term = $(this).val();
+                base.$el.find(".all_ingredients li").each(function () {
+                    var elt = $(this);
+                    console.log(elt.find(".ingredient_name").html());
+                    if (elt.find(".ingredient_name").html().toLowerCase().indexOf(search_term.toLowerCase()) === 0 || search_term === "") {
+                        elt.show();
+                    } else {
+                        elt.hide();
                     }
                 });
             });
