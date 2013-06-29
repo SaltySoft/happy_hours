585541146928c0a155d22855403b9d109622a725
Antoine Jackson <a.j.william26@gmail.com>
2013-06-29 03:33:06 +0200

    * Updated backend

--- HhBackend/DataAccess/T_Ingredient.cs
+++ HhBackend/DataAccess/T_Ingredient.cs
@@ -21,6 +21,7 @@ namespace HhBackend.DataAccess
     
         public int id { get; set; }
         public string name { get; set; }
+        public int alcool { get; set; }
     
         public virtual ICollection<T_CocktailsIngredients> T_CocktailsIngredients { get; set; }
     }
